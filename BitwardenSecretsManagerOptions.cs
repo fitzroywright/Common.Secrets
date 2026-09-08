b32c39d16dbbdf649a3652b0c3811aafed34f286
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-08 01:24:03 -0500

    Add Bitwarden Secrets Manager options

---
+++ BitwardenSecretsManagerOptions.cs
@@ -0,0 +1,12 @@
+namespace Common.Secrets;
+
+public sealed class BitwardenSecretsManagerOptions
+{
+    public bool Enabled { get; init; }
+    public string CliPath { get; init; } = "bws";
+    public string AccessTokenEnvironmentVariable { get; init; } = "BWS_ACCESS_TOKEN";
+    public string? ServerUrl { get; init; }
+    public TimeSpan CommandTimeout { get; init; } = TimeSpan.FromSeconds(15);
+    public IReadOnlyDictionary<string, string> SecretIds { get; init; }
+        = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+}
